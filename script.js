ed217e5ef12c03c3b598bd16a25a45506de8dd98
Josh Sparkes <jshsparx@gmail.com>
2022-06-07 10:40:52 -0600

    basic animation set up for sprite

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ const playerImage = new Image();
 playerImage.src = 'https://www.frankslaboratory.co.uk/downloads/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
-
+let frameX = 0;
+let frameY = 5;
+let gameFrame = 0;
+const staggerFrames = 5;
 
 function animate() {
   ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.fillRect(50,50,100,100);
   // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-  ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  if (gameFrame % staggerFrames == 0) {
+
+    if (frameX < 4) {
+      frameX++;
+    } else {
+      frameX = 0;
+    };
+  }
+  gameFrame++;
   requestAnimationFrame(animate);
 };
 
